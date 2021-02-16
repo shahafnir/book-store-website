6349a29438427c05b80f9b9d535a5d9f7446c836
Nir Shahaf <nirshahaf@Nirs-MacBook-Pro.local>
2021-02-16 18:49:06 +0200

    Add books service

--- src/app/books/book.model.ts
+++ src/app/books/book.model.ts
@@ -1,6 +1,11 @@
 export class Book {
-  constructor(public title: String, public publisher: String) {
+  constructor(
+    public title: String,
+    public author: String,
+    public publisher: String
+  ) {
     this.title = title;
+    this.author = author;
     this.publisher = publisher;
   }
 }
